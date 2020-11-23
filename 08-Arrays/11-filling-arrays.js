b346c5763682ba8e3c3d317ef82f797329f081ed
Gary Hake <Gary.Hake@waystar.com>
2020-11-23 07:00:57 -0500

    renamed array folder moved bankist app to projects folder more ways of creating/filling arrays

---
+++ 08-Arrays/11-filling-arrays.js
@@ -0,0 +1,31 @@
+// 2 ways we have so far
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+
+// Empty arrays + fill method
+// creates array w/ 7 empty spots
+const x = new Array(7);
+console.log(x);
+
+// can't really use this array
+console.log(x.map(() => 5));
+
+// except for fill method
+// mutates the array
+//x.fill(1);
+x.fill(1, 3, 5); // begin parameter
+console.log(x);
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+arr.fill(23, 2, 6);
+console.log(arr);
+
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);

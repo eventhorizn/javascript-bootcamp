d89e556af0639701956a0b02f7e6f8a2d96302e1
Gary Hake <Gary.Hake@waystar.com>
2020-11-23 18:59:20 -0500

    remainder operator

---
+++ 09-Numbers-Dates-Timers/03-remainder-operator.js
@@ -0,0 +1,15 @@
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3);// 8 = 2 * 3 + 2
+
+// Big check is if a number is even or odd
+// Good if you want to color rows
+console.log(6 % 2); // 0
+console.log(7 % 2); // 1
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));

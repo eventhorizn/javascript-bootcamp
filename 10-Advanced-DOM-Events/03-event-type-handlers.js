1b9323f79fa2eebf7fbee9509f02f148ecfb9cf9
Gary Hake <Gary.Hake@waystar.com>
2020-11-28 15:39:27 -0500

    event types and handlers

--- 10-Advanced-DOM-Events/03-event-type-handlers.js
+++ 10-Advanced-DOM-Events/03-event-type-handlers.js
@@ -33,14 +33,26 @@ const randomColor = () =>
 // This shows how bubbling works
 // Remove the jump on the nav__link
 // Clicking the nav__link will bubble up the click events for parent elements
+
+// target is the same, but currentTarget changes
+// currentTarget is the same as 'this'
+// The parameter after the function is a boolean
+// it's the 'capture' setting, which is only used for historical purposes
 document.querySelector('.nav__link').addEventListener('click', function (e) {
     this.style.backgroundColor = randomColor();
+    console.log('LINK', e.target, e.currentTarget);
+
+    // Stop propogation
+    // potentially dangerous
+    // e.stopPropagation();
 });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
     this.style.backgroundColor = randomColor();
+    console.log('CONTAINER', e.target, e.currentTarget);
 });
 
 document.querySelector('.nav').addEventListener('click', function (e) {
     this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
 });

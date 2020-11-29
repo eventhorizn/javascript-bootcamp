a06d871830fb727698ffde05f8478ce171da45c5
Gary Hake <Gary.Hake@waystar.com>
2020-11-29 15:07:17 -0500

    menu fade and passing arguments to event handlers really, .bind method and setting 'this'

--- 10-Advanced-DOM-Events/script.js
+++ 10-Advanced-DOM-Events/script.js
@@ -6,6 +6,14 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+/**
+ * @type {HTMLDivElement}
+ */
+const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
@@ -93,10 +101,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Tabbed component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -112,3 +116,24 @@ tabsContainer.addEventListener('click', function (e) {
   tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+///////////////////////////////////////
+// Menu fade animation
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// Passing 'argument' into handler
+// Really, just setting the 'this' keyword
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));

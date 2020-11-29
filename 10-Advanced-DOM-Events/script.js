a34f6ef8e5e9e4c2b00eba8f89430455940b0885
Gary Hake <Gary.Hake@waystar.com>
2020-11-28 20:52:33 -0500

    event delgation and page nav

--- 10-Advanced-DOM-Events/script.js
+++ 10-Advanced-DOM-Events/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,8 +34,6 @@ document.addEventListener('keydown', function (e) {
 
 ///////////////////////////////////////
 // Button scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
@@ -59,3 +59,33 @@ btnScrollTo.addEventListener('click', function (e) {
   // New school way, only in modern browsers
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+///////////////////////////////////////
+// Page navigation
+
+// The issue w/ this implementation is adding the event handler to each element
+// So, put the event listener to a common parent
+
+// document.querySelectorAll('.nav__link')
+//   .forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//       e.preventDefault();
+
+//       const id = this.getAttribute('href');
+//       document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//     });
+//   });
+
+
+// Event Delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+})

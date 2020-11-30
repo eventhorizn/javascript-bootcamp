66ad17e7a242ccdfed46d77da7d92192c1417768
Gary Hake <Gary.Hake@waystar.com>
2020-11-30 07:27:40 -0500

    lazy loading images

--- 10-Advanced-DOM-Events/script.js
+++ 10-Advanced-DOM-Events/script.js
@@ -174,7 +174,8 @@ const revealSection = function (entries, observer) {
   if (!entry.isIntersecting) return;
 
   entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target); // doesn't keep observing on scroll
+  // doesn't keep observing on scroll, for performance
+  observer.unobserve(entry.target);
 }
 
 const sectionObserver = new IntersectionObserver(
@@ -189,3 +190,30 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src w/ data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+}
+
+const imgObserver = new IntersectionObserver(
+  loadImg,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px'
+  }
+);
+
+imgTargets.forEach(x => imgObserver.observe(x));

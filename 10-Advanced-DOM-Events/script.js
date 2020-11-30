70fad3b4348e3db5ac6b934141bdd82349fddf34
Gary Hake <Gary.Hake@waystar.com>
2020-11-30 07:14:48 -0500

    reveal elements on scroll

--- 10-Advanced-DOM-Events/script.js
+++ 10-Advanced-DOM-Events/script.js
@@ -162,3 +162,30 @@ const headerObserver = new IntersectionObserver(
 });
 
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); // doesn't keep observing on scroll
+}
+
+const sectionObserver = new IntersectionObserver(
+  revealSection,
+  {
+    root: null,
+    threshold: 0.15,
+  }
+);
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});

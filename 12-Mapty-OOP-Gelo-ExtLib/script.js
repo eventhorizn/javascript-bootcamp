908ab2f84d9ab857955c1d3bf588eacac8f52a2a
Gary Hake <Gary.Hake@waystar.com>
2020-12-04 08:31:36 -0500

    mapty app feature complete. fun app

--- 12-Mapty-OOP-Gelo-ExtLib/script.js
+++ 12-Mapty-OOP-Gelo-ExtLib/script.js
@@ -76,8 +76,13 @@ class App {
     #workouts = [];
 
     constructor() {
+        // Get user's position
         this._getPosition();
 
+        // Get data from local storage
+        this._getLocalStorage();
+
+        // Attach event handlers
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
         containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -105,6 +110,10 @@ class App {
 
         // Handling clicks on map
         this.#map.on('click', this._showForm.bind(this));
+
+        this.#workouts.forEach(x => {
+            this._renderWorkoutMarker(x);
+        });
     }
 
     _showForm(mapE) {
@@ -179,6 +188,9 @@ class App {
 
         // Hide form + clear input fields
         this._hideForm();
+
+        // Set local storage to all workouts
+        this._setLocalStorage();
     }
 
     /**
@@ -265,7 +277,31 @@ class App {
         });
 
         // using the public interface
-        workout.click();
+        // This doesn't work w/ local storage unless you manually build your objects manually when loading
+        //workout.click();
+    }
+
+    _setLocalStorage() {
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    }
+
+    // If you want prototypal inheritance to work, manually build
+    // your objects as you read from data
+    _getLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('workouts'));
+
+        if (!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach(x => {
+            this._renderWorkout(x);
+        });
+    }
+
+    reset() {
+        localStorage.removeItem('workouts');
+        location.reload();
     }
 }
 const app = new App();

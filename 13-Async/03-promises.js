14a7507ff90f4fbdd17b3923608fb67862d31bf9
Gary Hake <Gary.Hake@waystar.com>
2020-12-07 06:51:37 -0500

    consuming promises

--- 13-Async/03-promises.js
+++ 13-Async/03-promises.js
@@ -7,7 +7,7 @@ const countriesContainer = document.querySelector('.countries');
 
 // Old school way
 
-const getCountryData = function (country) {
+const getCountryDataOld = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
     request.send();
@@ -35,7 +35,40 @@ const getCountryData = function (country) {
     });
 }
 
+const renderCountry = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
+
 
 // Modern Way
-const request = fetch(`https://restcountries.eu/rest/v2/name/portugal`);
-console.log(request);
+const getCountryDataVerbose = function (country) {
+    const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+        .then(function (response) {
+            return response.json(); // this is also a promise
+        })
+        .then(function (data) { // handle resonse.json promis
+            renderCountry(data[0]);
+        });
+}
+//getCountryDataVerbose('portugal');
+
+// Cleaner way to do the above
+const getCountryData = function (country) {
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+        .then(response => response.json())
+        .then(data => renderCountry(data[0]));
+}
+getCountryData('portugal');

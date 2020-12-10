14f5b97df1f798d87d45fcf48de75478e3db6d3f
Gary Hake <Gary.Hake@waystar.com>
2020-12-10 09:43:09 -0500

    common js which will hopefully be replaced by es modules

---
+++ 14-Modern-JS-Tooling/03-common-js-modules.js
@@ -0,0 +1,16 @@
+// Common JS Modules
+// Been used in node.js for all of it's existence
+// ES Modules just recently in node
+// Almost all modules in npm are common js modules (npm only node initially)
+
+// one file one module
+
+// Export
+// Doesn't work in browser, but works in node.js
+// export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+// };
+
+//Import
+//const {addToCart} = require('./shoppingCart.js');

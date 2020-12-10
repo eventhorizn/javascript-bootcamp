7982789963dc581ec59c13b40608bc53a0b5d6fe
Gary Hake <Gary.Hake@waystar.com>
2020-12-10 14:47:27 -0500

    bundling w/ parcel and npm scripts

--- 14-Modern-JS-Tooling/script.js
+++ 14-Modern-JS-Tooling/script.js
@@ -1,4 +1,6 @@
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// Don't need to do it this way
+//import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
     cart: [
@@ -16,3 +18,14 @@ const stateDeepClone = cloneDeep(state);
 state.user.loggedIn = false;
 console.log(stateClone);
 console.log(stateDeepClone);
+
+import add, { cart } from './shoppingCart.js'
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+console.log(cart);
+
+// keeps your state when saving and reloading
+if (module.hot) {
+    module.hot.accept();
+}
